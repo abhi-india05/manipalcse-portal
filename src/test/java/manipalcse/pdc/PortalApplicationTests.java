5a972a33b6925f0bcd5c5ed5cdb395a1c6cbe330
abhi-india05 <abhishekjholla@gmail.com>
2025-07-04 00:26:21 +0530

    initial commit: initialized repo, spring initializr setup

---
+++ src/test/java/manipalcse/pdc/PortalApplicationTests.java
@@ -0,0 +1,13 @@
+package manipalcse.pdc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
